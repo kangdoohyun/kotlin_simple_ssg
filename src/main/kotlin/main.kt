3c362697a0700814403650a92a1599444119879b
kangdoohyun <rkdengus1208@gmail.com>
2021-04-27 23:11:28 +0900

    필요없는 코드 삭제및 수정

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,212 +1,184 @@
+import java.lang.NumberFormatException
 import java.text.SimpleDateFormat
 
 fun main() {
-    println("== SIMPLE SSG 시작 ==")
     articleRepository.makeTestArticles()
-    while (true) {
-        print("명령어) ")
-        val command = readLineTrim() // 입력 : /article/detail?id=1&title=제목1
 
+    while (true){
+        print("명령어 : ")
+        val command = readLineTrim()
         val rq = Rq(command)
-
         when(rq.actionPath){
             "/system/exit" ->{
                 println("프로그램 종료")
                 break
-            }
-            "/article/detail"->{
-                val id = rq.getIntParam("id", 0)
-                var articleToDetail = articleRepository.getArticleById(id)
-
-                if (articleToDetail == null){
-                    println("${id}번 게시물은 존재하지 않습니다")
-                    continue
-                }
-                println("번호 : ${articleToDetail.id}")
-                println("작성 날짜 : ${articleToDetail.regDate}")
-                println("수정 날짜 : ${articleToDetail.updateDate}")
-                println("제목 : ${articleToDetail.title}")
-                println("내용 : ${articleToDetail.body}")
-            }
-            "/article/modify" ->{
-                val id = rq.getIntParam("id", 0)
-                var articleToModify = articleRepository.getArticleById(id)
-
-                if (articleToModify == null){
-                    println("${id}번 게시물은 존재하지 않습니다")
-                    continue
-                }
-                print("수정할 제목 :")
-                articleToModify.title = readLineTrim()
-                print("수정할 내용 :")
-                articleToModify.body = readLineTrim()
-                articleToModify.updateDate = Util.getNowDateStr()
-                println("${id}번 게시물을 수정하였습니다")
-            }
-            "/article/delete" ->{
-                val id = rq.getIntParam("id", 0)
-                val articleToDelete = articleRepository.getArticleById(id)
-
-                if (articleToDelete == null) {
-                    println("${id}번 게시물은 존재하지 않습니다")
-                    continue
-                }
-                    articleRepository.articles.remove(articleToDelete)
-                println("${id}번 게시물을 삭제하였습니다")
             }
             "/article/write" ->{
                 print("제목 : ")
                 val title = readLineTrim()
                 print("내용 : ")
                 val body = readLineTrim()
-
                 val id = articleRepository.addArticle(title, body)
-                println("${id}번 게시물이 작성되었습니다")
+                println("${id}번 게시물 작성 완료")
+            }
+            "/article/detail" -> {
+                val id = rq.getIntParam("id",0)
+                val article = articleRepository.getArticleById(id)
+                if(article == null){
+                    println("${id}번 게시물이 존재하지 않습니다")
+                    continue
+                }
+                println("번호 : ${article.id}")
+                println("작성 날짜 : ${article.regDate}")
+                println("수정 날짜 : ${article.updateDate}")
+                println("제목 : ${article.title}")
+                println("내용 : ${article.body}")
+            }
+            "/article/modify" ->{
+                val id = rq.getIntParam("id", 0)
+                val article = articleRepository.getArticleById(id)
+                if(article == null){
+                    println("${id}번 게시물이 존재하지 않습니다")
+                    continue
+                }
+                print("수정할 제목 : ")
+                article.title = readLineTrim()
+                print("수정할 내용 : ")
+                article.body = readLineTrim()
+                article.updateDate = Util.getNowDateStr()
+            }
+            "/article/delete" ->{
+                val id = rq.getIntParam("id", 0)
+                val article = articleRepository.getArticleById(id)
+                if(article == null){
+                    println("${id}번 게시물이 존재하지 않습니다")
+                    continue
+                }
+                articleRepository.articles.remove(article)
             }
             "/article/list" ->{
-                val commandBits = command.trim().split(" ")
-                var page = rq.getIntParam("page", 1)
-                var searchKeyword = rq.getStringParam("searchKeyword", "")
-                if (commandBits.size == 4) {
-                    searchKeyword = commandBits[2]
-                    page = commandBits[3].toInt()
-                } else if (commandBits.size == 3) {
-                    page = commandBits[2].toInt()
-                }
-                val itemsCountInAPage = 10
-                val offsetCount = (page - 1) * itemsCountInAPage
-                val filteredArticles = articleRepository.getFilteredArticles(searchKeyword, offsetCount, itemsCountInAPage)
-
+                val page = rq.getIntParam("page", 1)
+                val searchKeyword = rq.getStringParam("searchKeyword", "")
+                val itemsInAPage = 5
+                val jumpIndex = (page -1) * itemsInAPage
+                val articles = articleRepository.getFilteredArticles(searchKeyword, jumpIndex, itemsInAPage)
                 println("번호 / 작성날짜 / 제목")
-                for (article in filteredArticles) {
-                    println("${article.id} / ${article.regDate} / ${article.title}")
+                for (article in articles){
+                    println("${article.id}  /  ${article.regDate}  /  ${article.title}")
                 }
+            }
+            else->{
+                println("${rq.actionPath}는 존재하지 않는 명령어입니다")
             }
         }
     }
-
-    println("== SIMPLE SSG 끝 ==")
 }
+fun readLineTrim() = readLine()!!.trim()
 
-class Rq(command: String) {
-
-    val actionPath: String
-    val paramMap: Map<String, String>
-
-    init {
-        val commandBits = command.split("?", limit = 2)
-
-        actionPath = commandBits[0].trim()
-        val queryStr = if (commandBits.lastIndex == 1 && commandBits[1].isNotEmpty()) {
-            commandBits[1].trim()
-        } else {
-            ""
-        }
-
-        paramMap = if (queryStr.isEmpty()) {
-            mapOf()
-        } else {
-            val paramMapTemp = mutableMapOf<String, String>()
-            val queryStrBits = queryStr.split("&")
-
-            for (queryStrBit in queryStrBits) {
-                val queryStrBitBits = queryStrBit.split("=", limit = 2)
-                val paramName = queryStrBitBits[0]
-                val paramValue = if (queryStrBitBits.lastIndex == 1 && queryStrBitBits[1].isNotEmpty()) {
-                    queryStrBitBits[1].trim()
-                } else {
-                    ""
-                }
-
-                if (paramValue.isNotEmpty()) {
-                    paramMapTemp[paramName] = paramValue
-                }
-            }
-
-            paramMapTemp.toMap()
-        }
-    }
-    fun getStringParam(paramValue : String, default : String): String{
-        return paramMap[paramValue] ?: default
-    }
-    fun getIntParam(paramValue : String, default : Int): Int {
-        return try {
-            paramMap[paramValue]?.toInt()?:default
-        }
-        catch (e: NumberFormatException) {
-            default
-        }
-    }
-
-}
-data class Article(val id: Int, val regDate: String, var updateDate: String, var title: String, var body: String){}
+data class Article(val id : Int, val regDate : String, var updateDate : String, var title : String, var body : String){}
 
 object articleRepository{
-    val articles = mutableListOf<Article>()
-    var articleLastId = 0
+    var articlesLastId = 0
+    var articles = mutableListOf<Article>()
 
-    fun addArticle(title : String, body: String) : Int{
-        val id = ++articleLastId
+    fun addArticle(title: String, body: String) : Int{
+        val id = ++articlesLastId
         val regDate = Util.getNowDateStr()
         val updateDate = Util.getNowDateStr()
-
-        val article = Article(id, regDate, updateDate, title, body)
+        val article = Article(id,regDate,updateDate,title, body)
         articles.add(article)
-
-        articleLastId = id
-
+        articlesLastId = id
         return id
     }
-    fun getArticleById(id: Int): Article? {
-        for (article in articles) {
-            if (article.id == id) {
+
+    fun makeTestArticles(){
+        for (i in 1 .. 100){
+            val title = "제목_$i"
+            val body = "내용_$i"
+            addArticle(title, body)
+        }
+    }
+
+    fun getArticleById(id : Int) : Article?{
+        for (article in articles){
+            if(article.id == id){
                 return article
             }
         }
         return null
     }
-    fun makeTestArticles() {
-        for (id in 1..100) {
-            val title = "제목_$id"
-            val body = "내용_$id"
 
-            addArticle(title, body)
-        }
-    }
-    fun getFilteredArticles(searchKeyword: String, jumpIndex: Int, itemsInAPage: Int): MutableList<Article> {
-        var filtered1Articles = articles
-        if (searchKeyword.isNotEmpty()) {
-            filtered1Articles = mutableListOf<Article>()
-
-            for (article in articles) {
-                if (article.title.contains(searchKeyword)) {
-                    filtered1Articles.add(article)
+    fun getFilteredArticles(searchKeyword : String, jumpIndex : Int, itemsInAPage : Int): List<Article>{
+        var filteredArticles1 = articles
+        if(searchKeyword.isNotEmpty()){
+            filteredArticles1 = mutableListOf<Article>()
+            for (article in articles){
+                if(article.title.contains(searchKeyword)){
+                    filteredArticles1.add(article)
                 }
             }
         }
-        val filtered2Articles = mutableListOf<Article>()
-
-        val startIndex = filtered1Articles.lastIndex - jumpIndex
-        var endIndex = startIndex - itemsInAPage + 1
-
-        if (endIndex < 0) {
-            endIndex = 0
+        val startIndex = filteredArticles1.lastIndex - jumpIndex
+        val endIndex = startIndex - itemsInAPage + 1
+        val filteredArticles2 = mutableListOf<Article>()
+        for (i in startIndex downTo endIndex){
+            filteredArticles2.add(filteredArticles1[i])
         }
-        for (i in startIndex downTo endIndex) {
-            filtered2Articles.add(filtered1Articles[i])
-        }
-
-        return filtered2Articles
+        return filteredArticles2
     }
 }
 
-fun readLineTrim() = readLine()!!.trim()
-
-object Util {
-
+object Util{
     fun getNowDateStr(): String {
-        val dateFormat = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-
-        return dateFormat.format(System.currentTimeMillis())
+        val format1 = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        return format1.format(System.currentTimeMillis())
     }
 }
+class Rq(command: String) {
+    val actionPath : String
+    private val paramMap : Map<String, String>
+
+    init {
+        val commandBits = command.split("?", limit = 2)
+        actionPath = commandBits[0].trim()
+        val queryStr = if(commandBits.lastIndex == 1 && commandBits[1].isNotEmpty()){
+            commandBits[1].trim()
+        }
+        else{
+            ""
+        }
+        paramMap = if(queryStr.isEmpty()){
+            mapOf()
+        }
+        else{
+            val queryStrBits = queryStr.split("&")
+            val paramMapTemp = mutableMapOf<String, String>()
+            for (queryStrBit in queryStrBits){
+                val queryStrBitsBits = queryStrBit.split("=", limit = 2)
+                val paramKey = queryStrBitsBits[0].trim()
+                val paramValue = if(queryStrBitsBits.lastIndex == 1 && queryStrBitsBits[1].isNotEmpty()){
+                    queryStrBitsBits[1].trim()
+                }
+                else{
+                    ""
+                }
+                if(paramValue.isNotEmpty()){
+                    paramMapTemp[paramKey] = paramValue
+                }
+            }
+            paramMapTemp.toMap()
+        }
+    }
+    fun getStringParam(paramValue : String, default : String): String {
+        return paramMap[paramValue] ?: default
+    }
+    fun getIntParam(paramValue : String, default: Int) : Int {
+        return try {
+            paramMap[paramValue]?.toInt() ?: default
+        }
+        catch (e:NumberFormatException){
+            default
+        }
+    }
+}
+
