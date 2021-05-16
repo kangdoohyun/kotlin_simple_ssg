a5e38c338b052e86652f0ccec21020d262369f47
kangdoohyun <rkdengus1208@gmail.com>
2021-05-16 22:17:18 +0900

    SSGController 다시만들기

--- src/main/kotlin/SSGController.kt
+++ src/main/kotlin/SSGController.kt
@@ -6,7 +6,6 @@ class SSGController {
 
     private fun makeArticleListPages() {
         val boards = boardRepository.getBoards()
-
         for (board in boards){
             makeArticleListPage(board)
         }
@@ -14,15 +13,13 @@ class SSGController {
 
     private fun makeArticleListPage(board: Board) {
         val articles = articleRepository.getArticles()
-        val boards = boardRepository.getBoards()
-
+        val newBoards = boardRepository.getBoards()
         val filePath = "html/list/article_list_${board.code}.html"
         var fileContent = "<meta charset=\"UTF-8\">\n"
         fileContent += "<div><h1>${board.name}게시판</h1></div>\n"
-        fileContent += "<div><h2>게시판 리스트</h2></div>\n"
-
-        for (board1 in boards){
-            fileContent += "<div><a href=\"article_list_${board1.code}.html\">${board1.name}게시판</a></div>\n"
+        fileContent += "<div><h2>게시판 종류</h2></div>\n"
+        for (newBoard in newBoards){
+            fileContent += "<div><a href=\"article_list_${newBoard.code}.html\">${newBoard.name}게시판</a></div>\n"
         }
         fileContent += "<div><h2>${board.name}게시글</h2></div>\n"
         for (article in articles){
@@ -32,37 +29,29 @@ class SSGController {
             }
         }
         writeStrFile(filePath, fileContent)
-        println("${filePath}파일이 생성되었습니다")
+        println("${filePath}파일 생성 완료")
     }
 
     private fun makeArticleDetailPages() {
         val articles = articleRepository.getArticles()
-
         for (article in articles){
             makeArticleDetailPage(article)
         }
     }
 
     private fun makeArticleDetailPage(article: Article) {
-        val member = memberRepository.getMemberById(article.memberId)!!
-        val board = boardRepository.getBoardById(article.boardId)!!
-
         val filePath = "html/detail/article_detail_${article.id}.html"
-
+        val member = memberRepository.getMemberById(article.memberId)!!
         var fileContent = "<meta charset=\"UTF-8\">\n"
         fileContent += "<div><h1>${article.title}</h1></div>\n"
         fileContent += "<div>번호 : ${article.id}</div>\n"
-        fileContent += "<div>작성 날짜 : ${article.regDate}</div>\n"
-        fileContent += "<div>수정 날짜 : ${article.updateDate}</div>\n"
-        fileContent += "<div>작성자 : ${member.nickname}</div>\n"
-        fileContent += "<div>게시판 : ${board.name}</div>\n"
         fileContent += "<div>제목 : ${article.title}</div>\n"
         fileContent += "<div>내용 : ${article.body}</div>\n"
+        fileContent += "<div>작성자 : ${member.nickname}</div>\n"
         fileContent += "<div><a href=\"#\" onclick=\"history.back();\">뒤로가기</a></div>\n"
 
         writeStrFile(filePath, fileContent)
-        println("${filePath}파일이 생성되었습니다")
+        println("${filePath}파일 생성 완료")
     }
-
 
 }
